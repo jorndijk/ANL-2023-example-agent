141011a9252642b1aeec63dca58a0e575ad87a65
Jorn <J.W.Dijk@student.tudelft.nl>
2023-03-30 12:14:09 +0200

    add venv to gitignore

--- run.py
+++ run.py
@@ -17,13 +17,17 @@ if not RESULTS_DIR.exists():
 #   You need to specify a time deadline (is milliseconds (ms)) we are allowed to negotiate before we end without agreement
 settings = {
     "agents": [
+        #{
+        #    "class": "agents.ANL2022.dreamteam109_agent.dreamteam109_agent.DreamTeam109Agent",
+        #    "parameters": {"storage_dir": "agent_storage/DreamTeam109Agent"},
+        #},
         {
-            "class": "agents.ANL2022.dreamteam109_agent.dreamteam109_agent.DreamTeam109Agent",
-            "parameters": {"storage_dir": "agent_storage/DreamTeam109Agent"},
-        },
-        {
-            "class": "agents.template_agent.template_agent.TemplateAgent",
+            "class": "agents.agent_group_49.agent_group_49.TemplateAgent",
             "parameters": {"storage_dir": "agent_storage/TemplateAgent"},
+        },
+{
+            "class": "agents.stupid_agent.stupid_agent.StupidAgent",
+            "parameters": {"storage_dir": "agent_storage/StupidAgent"},
         },
     ],
     "profiles": ["domains/domain00/profileA.json", "domains/domain00/profileB.json"],
